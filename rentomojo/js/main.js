cd508324e49773daf3281ba61e35f9c40d503a3a
Raman Choudhary <raman.choudhary65@gmail.com>
2017-05-11 19:26:35 +0530

    impoved mobile

--- rentomojo/js/main.js
+++ rentomojo/js/main.js
@@ -1,3 +1,10 @@
+var isMenuOpen = false;
+
 $('.hamburger-menu').click((e)=>{
+    !isMenuOpen 
+        ? $(e.target).removeClass('fa-bars').addClass('fa-remove')
+        : $(e.target).removeClass('fa-remove').addClass('fa-bars')
+    
 	$('.left-navigation').toggleClass('open');
+    isMenuOpen = !isMenuOpen;
 });
